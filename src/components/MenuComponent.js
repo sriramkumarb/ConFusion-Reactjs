4b80795f17a5c2c1a259b975ec3f65d09dbe2ec5
Sriram <sriram9159541445@gmail.com>
2020-05-08 13:59:20 +0530

    Week 1 assignment

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
+import DishdetailComponent from './DishdetailComponent';
 
 class Menu extends Component {
     constructor(props) {
@@ -17,13 +18,7 @@ class Menu extends Component {
     renderDish(dish){
       if ( dish != null){
         return(
-          <Card>
-            <CardImg width="100%" src={dish.image} alt={dish.name} />
-            <CardBody>
-              <CardTitle>{dish.name}</CardTitle>
-              <CardText>{dish.description}</CardText>
-            </CardBody>
-          </Card>
+          <DishdetailComponent dish={dish}></DishdetailComponent>
         )
       }
       else {
@@ -52,11 +47,7 @@ class Menu extends Component {
             <div className="row">
                   {menu}
             </div>
-            <div className="row">
-              <div className="col-12 m-1">
-                {this.renderDish(this.state.selectedDish)}
-              </div> 
-            </div>
+            {this.renderDish(this.state.selectedDish)}
           </div>
         );
     }
