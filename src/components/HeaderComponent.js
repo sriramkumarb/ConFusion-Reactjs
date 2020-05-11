0c30894aabe03b8ef6e6f41f0ec9507c21eab228
Sriram <sriram9159541445@gmail.com>
2020-05-11 16:51:49 +0530

    Single page applications part-1

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,14 +1,58 @@
 import React, {Component} from 'react';
-import { Navbar, NavbarBrand, Jumbotron } from 'reactstrap';
+import { Navbar, NavbarBrand, Nav, NavbarToggler, Collapse, NavItem, Jumbotron } from 'reactstrap';
+import {NavLink} from 'react-router-dom';
 
 class Header extends Component {
+
+    constructor(props) {
+        super(props);
+        
+        this.toggleNav = this.toggleNav.bind(this);
+        this.state={
+            isNavOpen: false
+        }
+    }
+    toggleNav() {
+        this.setState({
+            isNavOpen: ! this.state.isNavOpen
+        });
+    }
+
     render(){
         return(
            <React.Fragment> 
                 {/* we can also use  <React.Fragment></React.Fragment> instead of <></> */}
-                <Navbar dark>
+                <Navbar dark expand="md">
                     <div className="container">
-                        <NavbarBrand href="#">Ristorante Con Fusion</NavbarBrand>
+                        <NavbarToggler onClick={this.toggleNav} />
+                        <NavbarBrand className="mr-auto" href="/home">
+                            <img src="assets/images/logo.png" height="30" width="41"
+                              alt="Ristorante Con Fusion" />
+                        </NavbarBrand>
+                        <Collapse isOpen={this.state.isNavOpen} navbar>
+                            <Nav navbar>
+                                <NavItem>
+                                    <NavLink className="nav-link" to="/home">
+                                        <span className="fa fa-lg fa-home"></span> Home
+                                    </NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink className="nav-link" to="/aboutus">
+                                        <span className="fa fa-lg fa-info"></span> About Us
+                                    </NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink className="nav-link" to="/menu">
+                                        <span className="fa fa-lg fa-list"></span> Menu
+                                    </NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink className="nav-link" to="/contactus">
+                                        <span className="fa fa-lg fa-address-card"></span> Contact Us
+                                    </NavLink>
+                                </NavItem>
+                            </Nav>
+                        </Collapse>
                     </div>
                 </Navbar>
 
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -35,8 +35,7 @@
 // implementing menu component via fuctional methods
 
 import React from 'react';
-import { Card, CardImg, CardImgOverlay,
-    CardTitle } from 'reactstrap';
+import { Card, CardImg, CardImgOverlay,CardTitle } from 'reactstrap';
 
     function RenderMenuItem ({dish, onClick}) {
         return (
