d9149ec654c981ca060234964ae5b5051944a337
Sriram <sriram9159541445@gmail.com>
2020-05-17 12:50:24 +0530

    use of npm run build command

---
+++ build/precache-manifest.3d312fb56ef3e7452e67346e854c545b.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78f0e9cd1784df8cc93edf7fa9285137",
+    "url": "/index.html"
+  },
+  {
+    "revision": "887cb07feb2b1d57e58a",
+    "url": "/static/css/2.9a81eafb.chunk.css"
+  },
+  {
+    "revision": "a105603553e0b61946e9",
+    "url": "/static/css/main.847e23bc.chunk.css"
+  },
+  {
+    "revision": "887cb07feb2b1d57e58a",
+    "url": "/static/js/2.6e657f9c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.6e657f9c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a105603553e0b61946e9",
+    "url": "/static/js/main.fabd5234.chunk.js"
+  },
+  {
+    "revision": "a35616d865ebcd47e92d",
+    "url": "/static/js/runtime-main.185a88bb.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
